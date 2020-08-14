07ed45e73fb7016c96a407e8aaf6a7867cac321e
Hassan <hassanalihazaraa@gmail.com>
2020-08-14 14:42:14 +0200

    created showmyname action and showmyname twig file

--- src/Controller/LearningController.php
+++ src/Controller/LearningController.php
@@ -3,17 +3,24 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LearningController extends AbstractController
 {
     /**
-     * @Route("/learning", name="learning")
+     * @Route("/about", name="about-me")
      */
-    public function index()
+    public function aboutMe(): Response
     {
-        return $this->render('learning/index.html.twig', [
-            'controller_name' => 'LearningController',
-        ]);
+        return $this->render('learning/about-me.html.twig', ['name' => 'Becode']);
+    }
+
+    /**
+     * @Route("/", name="my-name")
+     */
+    public function showMyName()
+    {
+        return $this->render('learning/show-myname.html.twig', ['showName' => 'Hassan']);
     }
 }
