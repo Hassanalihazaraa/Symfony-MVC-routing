b52c9af1c340ad2c48412c89518e6c6e78095a67
Hassan <hassanalihazaraa@gmail.com>
2020-08-14 15:35:05 +0200

    forwarding to homepage complete

--- src/Controller/LearningController.php
+++ src/Controller/LearningController.php
@@ -4,23 +4,31 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LearningController extends AbstractController
 {
     /**
      * @Route("/about-becode", name="about")
+     * @param SessionInterface $session
+     * @return Response
      */
-    public function aboutMe(): Response
+    public function aboutMe(SessionInterface $session): Response
     {
-        return $this->render('learning/about-me.html.twig', ['name' => 'Becode']);
+        if ($session->get('name')) {
+            return $this->render('learning/about-me.html.twig', ['name' => $session->get('name')]);
+        } else {
+            return $this->forward('App\Controller\LearningController::showMyName');
+        }
     }
 
     /**
      * @Route("/", name="show-name")
      */
-    public function showMyName()
+    public function showMyName(): Response
     {
+
         return $this->render('learning/show-myname.html.twig', ['showName' => 'Unknown']);
     }
 }
