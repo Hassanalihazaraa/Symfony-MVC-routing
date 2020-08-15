9a305cfa979d105d474153c6497604ecd21760cd
Hassan <hassanalihazaraa@gmail.com>
2020-08-15 19:37:55 +0200

    created a user entity and recreated the form with symfony's built in form object and saved data to database

--- src/Controller/LearningController.php
+++ src/Controller/LearningController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -18,12 +21,10 @@ class LearningController extends AbstractController
      */
     public function aboutMe(SessionInterface $session): Response
     {
-        if ($session->get('fullname')) {
-            $name = $session->get('fullname');
-            return $this->render('learning/about-me.html.twig', ['name' => $name]);
+        if ($session->get('name')) {
+            return $this->render('learning/about-me.html.twig', ['name' => $session->get('name')]);
         }
         return $this->forward('App\Controller\LearningController::showMyName');
-
     }
 
     /**
@@ -33,24 +34,44 @@ class LearningController extends AbstractController
      */
     public function showMyName(SessionInterface $session): Response
     {
-        if ($session->get('fullname')) {
-            $name = $session->get('fullname');
+        if ($session->get('name')) {
+            $name = $session->get('name');
         } else {
             $name = 'unknown';
         }
-        return $this->render('learning/show-myname.html.twig', ['name' => $name]);
+        $user = new User();
+        //create a form
+        $form = $this->createFormBuilder($user)
+            ->setAction($this->generateUrl('change-name'))
+            ->setMethod('POST')
+            //create input type text
+            ->add('name', TextType::class, [
+                'attr' => ['placeholder' => 'Enter your name', 'class' => 'w-50 text-center mb-2']
+            ])
+            //create input type submit button
+            ->add('save', SubmitType::class, ['label' => 'Add', 'attr' => ['class' => 'btn btn-primary w-50']])
+            ->getForm();
+        //return the form view
+        return $this->render('learning/show-myname.html.twig', [
+            'name' => $name,
+            'form' => $form->createView()
+        ]);
     }
 
     /**
-     * @Route("/changeName", name="change-name", methods={"POST"})
+     * @Route("/changeName", name="change-name")
      * @param Request $request
      * @param SessionInterface $session
+     * @return RedirectResponse
      */
     public function changeMyName(Request $request, SessionInterface $session): RedirectResponse
     {
-        $name = $request->request->get('fullname');
-        $session->set('fullname', $name);
-
+        $form = $request->request->get('form');
+        $user = new User();
+        $user->setName($form['name']);
+        $this->getDoctrine()->getManager()->persist($user);
+        $this->getDoctrine()->getManager()->flush();
+        $session->set('name', $form['name']);
         return $this->redirectToRoute('show-name');
     }
 }
